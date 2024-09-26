a36f412c9f5b319d86380275e4d25676109e7cd4
roro <rorosonick@gmail.com>
2024-09-26 23:16:52 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { userState, useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+let todoItemId = 0;
+
+const TodoItemInputField = (props) => {
+  const [input, setInput] = useState("");
+  const onSubmit = () => {
+    props.onSubmit(input);
+    setInput("");
+  }
+  return (
+    <div>
+      <TextField
+        label="할일여기다써라"
+        variant="outlined"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <Button variant="outlined" onClick={onSubmit} >전송</Button>
+    </div>
+  );
+};
+
+const TodoItemList = (props) => {
+  const todoList = props.todoItemList.map((todoItem, index) => {
+    return <li key={index}>{todoItem.todoItemContent}</li>;
+  });
+  return (<div>
+    <ul>{todoList}</ul>
+  </div>);
+};
+
 
 function App() {
+  const [todoItemList, setTodoItemList] = useState([]);
+
+  const onSubmit = (newTodoItem) => {
+    setTodoItemList([...todoItemList, {
+      id: todoItemId++,
+      todoItemContent: newTodoItem,
+      isFinished: false,
+    }]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello Taeyang!!ss
-        </a>
-      </header>
+      <TodoItemInputField onSubmit={onSubmit} />
+      <TodoItemList todoItemList={todoItemList} />
     </div>
   );
 }
+
 
 export default App;
